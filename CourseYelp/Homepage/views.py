414ee56d84e233dca75c6abf3b9543bc00382b0f
Tianpei Luo <tluo4@stevens.edu>
2016-06-02 08:58:29 -0400

    upload summer project file

---
+++ CourseYelp/Homepage/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from .forms import ContactForm,SignUpForm
+# Create your views here.
+
+def home(request):
+    title = 'Welcome'
+    # if request.user.is_authenticated():
+    #     title = "My Title %s" %(request.user)
+
+    #add a form
+    # if request.method == "POST":
+    #     print (request.POST)
+    form = SignUpForm(request.POST or None)
+
+    context = {
+        "title" : title,
+        "form" : form,
+    }
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        #print (request.POST['email'])
+        full_name = form.cleaned_data.get("full_name")
+        if not full_name:
+            full_name = "New full name"
+        instance.full_name = full_name
+        instance.save()
+
+        context = {
+            "title" : "Thank you"
+        }
+    return render(request,"home.html",context)
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        # for key in form.cleaned_data:
+        #     print (key)
+        #     print (form.cleaned_data.get(key))
+        # in python 2.x it is iteritems() but in 3.x it is just items() 
+        for key,value in form.cleaned_data.items():
+            print (key,value)
+        # email = form.cleaned_data.get("email")
+        # message = form.cleaned_data.get("message")
+        # full_name = form.cleaned_data.get("full_name")
+        # print email, message, full_name
+    context = {
+        "form": form,
+    }
+    return render(request,"forms.html",context)
